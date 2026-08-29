31e51c78256cec7665b06d1507f21f8c42778b5d
socialman121-art <socialman121@gmail.com>
2026-08-29 21:20:33 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBNYVfrhotHx-Anv_3s9CqZ_VdQe9j61F8",
+  authDomain: "ghbsssitaroad.firebaseapp.com",
+  projectId: "ghbsssitaroad",
+  storageBucket: "ghbsssitaroad.firebasestorage.app",
+  messagingSenderId: "947888082435",
+  appId: "1:947888082435:web:467a3749a9d9ed677b8836",
+  measurementId: "G-2XLE6CG7J2"
+};
