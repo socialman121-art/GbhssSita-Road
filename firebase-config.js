9f0f20c9da55eba8ef4e3da41697f584d479556b
socialman121-art <socialman121@gmail.com>
2026-08-30 02:05:36 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyBNWrthox-Anv_3s9CqZ_VdQe9Gjlf8",
-  authDomain: "gbhsssitaroad.firebaseapp.com",
-  projectId: "gbhsssitaroad",
-  storageBucket: "gbhsssitaroad.firebasestorage.app",
-  messagingSenderId: "94788082435",
-  appId: "1:94788082435:web:4b7a3474a9d9ed677b8836",
-  measurementId: "G-2LXLE6CG72"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBNYVfrhotHx-Anv_3s9CqZ_VdQe9j61F8",
+  authDomain: "ghbsssitaroad.firebaseapp.com",
+  projectId: "ghbsssitaroad",
+  storageBucket: "ghbsssitaroad.firebasestorage.app",
+  messagingSenderId: "947888082435",
+  appId: "1:947888082435:web:467a3749a9d9ed677b8836",
+  measurementId: "G-2XLE6CG7J2"
 };
